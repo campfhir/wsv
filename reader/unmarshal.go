8a3507290d16e5a16611788ffea283ff3002b265
Scott Eremia-Roden <scotteremiaroden@Mac.attlocal.net>
2025-09-04 02:31:51 -0700

    modified the unmarshal to allow formating attribute

--- reader/unmarshal.go
+++ reader/unmarshal.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/campfhir/wsv/internal"
-	utils "github.com/campfhir/wsv/internal"
 )
 
 type unmarshalError struct {
@@ -33,176 +32,7 @@ func (e *unmarshalError) Error() string {
 	return fmt.Sprintf("unmarshal error field: '%s' attributes: [%s], field index: %d, field type: %s", e.field, e.format, e.fieldIndex, e.fieldType)
 }
 
-// func unmarshalRow(fields []utils.Field, t reflect.Type) (*reflect.Value, error) {
-// 	val := reflect.New(t).Elem()
-// 	_type := val.Type()
-// 	if _type.Kind() != reflect.Struct {
-// 		return nil, errors.New("expected a struct to unmarshal to")
-// 	}
-// 	tagLookUp := make(map[string]fieldInfo)
-// 	collectFields(_type, nil, tagLookUp)
-
-// fl:
-// 	for fi, field := range fields {
-// 		_ = fi
-// 		fi, ok := tagLookUp[field.FieldName]
-// 		if !ok {
-// 			continue
-// 		}
-// 		ty := fi.Field
-
-// 		if !ty.IsExported() {
-// 			continue
-// 		}
-// 		tag, _ := ty.Tag.Lookup("wsv")
-// 		if tag == "" {
-// 			tag = ty.Name
-// 		}
-// 		name := ""
-// 		format := ""
-// 		keys := utils.SplitQuoted(tag)
-// 		if len(keys) > 0 {
-// 			name = keys[0]
-// 		}
-// 		if name == "" || name != field.FieldName {
-// 			continue
-// 		}
-// 		for _, key := range keys {
-// 			if f, ok := strings.CutPrefix(key, "format:"); ok {
-// 				format = f
-// 				break
-// 			}
-// 		}
-
-// 		sf := val.FieldByIndex(fi.Index)
-// 		if sf.CanAddr() {
-// 			addr := sf.Addr().Interface()
-// 			if u, ok := addr.(UnmarshalWSV); ok {
-// 				if err := u.UnmarshalWSV(field.Value, format); err != nil {
-// 					return nil, &unmarshalError{
-// 						tag:        name,
-// 						attributes: keys,
-// 						fieldIndex: fi.Index,
-// 						fieldType:  sf.Type().String(),
-// 						cause:      err,
-// 					}
-// 				}
-// 				continue fl
-// 			}
-// 		}
-// 		switch sf.Type().Kind() {
-// 		case reflect.String:
-// 			sf.SetString(field.Value)
-// 			continue fl
-// 		case reflect.Bool:
-// 			v, err := internal.ParseBool(field.Value)
-// 			if err != nil {
-// 				return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: sf.Kind().String(), cause: err}
-// 			}
-// 			sf.SetBool(v)
-// 			continue fl
-// 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-// 			v, err := strconv.ParseInt(field.Value, 10, strconv.IntSize)
-// 			if err != nil {
-// 				return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: sf.Kind().String(), cause: err}
-// 			}
-// 			sf.SetInt(v)
-// 			continue fl
-// 		case reflect.Float32, reflect.Float64:
-// 			v, err := strconv.ParseFloat(field.Value, strconv.IntSize)
-// 			if err != nil {
-// 				return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: sf.Kind().String(), cause: err}
-// 			}
-// 			sf.SetFloat(v)
-// 			continue fl
-// 		case reflect.Ptr:
-// 			if field.IsNull {
-// 				continue fl
-// 			}
-// 			switch sf.Interface().(type) {
-// 			case *time.Time:
-// 				v, err := time.Parse(format, field.Value)
-// 				if err != nil {
-// 					return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: "*time.Time", cause: err}
-// 				}
-// 				sf.Set(reflect.ValueOf(&v))
-// 				continue fl
-// 			case *string:
-// 				sf.Set(reflect.ValueOf(&field.Value))
-// 				continue fl
-// 			case *int:
-// 				v, err := strconv.ParseInt(field.Value, 10, strconv.IntSize)
-// 				if err != nil {
-// 					return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: "*int", cause: err}
-// 				}
-// 				x := int(v)
-// 				sf.Set(reflect.ValueOf(&x))
-// 				continue fl
-// 			case *int8:
-// 				v, err := strconv.ParseInt(field.Value, 10, strconv.IntSize)
-// 				if err != nil {
-// 					return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: "*int8", cause: err}
-// 				}
-// 				x := int8(v)
-// 				sf.Set(reflect.ValueOf(&x))
-// 				continue fl
-// 			case *int16:
-// 				v, err := strconv.ParseInt(field.Value, 10, strconv.IntSize)
-// 				if err != nil {
-// 					return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: "*int16", cause: err}
-// 				}
-// 				x := int16(v)
-// 				sf.Set(reflect.ValueOf(&x))
-// 				continue fl
-// 			case *int32:
-// 				v, err := strconv.ParseInt(field.Value, 10, strconv.IntSize)
-// 				if err != nil {
-// 					return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: "*int32", cause: err}
-// 				}
-// 				x := int32(v)
-// 				sf.Set(reflect.ValueOf(&x))
-// 				continue fl
-// 			case *int64:
-// 				v, err := strconv.ParseInt(field.Value, 10, strconv.IntSize)
-// 				if err != nil {
-// 					return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: "*int64", cause: err}
-// 				}
-// 				sf.Set(reflect.ValueOf(&v))
-// 				continue fl
-// 			case *float32:
-// 				v, err := strconv.ParseFloat(field.Value, strconv.IntSize)
-// 				if err != nil {
-// 					return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: "*float32", cause: err}
-// 				}
-// 				x := float32(v)
-// 				sf.Set(reflect.ValueOf(&x))
-// 				continue fl
-// 			case *float64:
-// 				v, err := strconv.ParseFloat(field.Value, strconv.IntSize)
-// 				if err != nil {
-// 					return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: "*float64", cause: err}
-// 				}
-// 				sf.Set(reflect.ValueOf(&v))
-// 				continue fl
-// 			case *bool:
-// 				v, err := internal.ParseBool(field.Value)
-// 				if err != nil {
-// 					return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: "*bool", cause: err}
-// 				}
-// 				sf.Set(reflect.ValueOf(&v))
-// 				continue fl
-// 			default:
-// 				return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: sf.Type().String()}
-// 			}
-// 		default:
-// 			return nil, &unmarshalError{tag: name, attributes: keys, fieldIndex: fi.Index, fieldType: sf.Type().String()}
-// 		}
-// 	}
-
-// 	return &val, nil
-// }
-
-func unmarshalRow(fields []utils.Field, t reflect.Type) (*reflect.Value, error) {
+func unmarshalRow(fields []internal.Field, t reflect.Type) (*reflect.Value, error) {
 	val := reflect.New(t).Elem()
 	if val.Kind() != reflect.Struct {
 		return nil, errors.New("expected a struct to unmarshal to")
@@ -246,27 +76,8 @@ func unmarshalRow(fields []utils.Field, t reflect.Type) (*reflect.Value, error) 
 	return &val, nil
 }
 
-// // parseWSVTag extracts the main name, format, and attributes from the struct tag.
-// func parseWSVTag(sf reflect.StructField) (name, format string, attrs []string) {
-// 	tag, _ := sf.Tag.Lookup("wsv")
-// 	if tag == "" {
-// 		tag = sf.Name
-// 	}
-// 	attrs = utils.SplitQuoted(tag)
-// 	if len(attrs) > 0 {
-// 		name = attrs[0]
-// 	}
-// 	for _, attr := range attrs {
-// 		if f, ok := strings.CutPrefix(attr, "format:"); ok {
-// 			format = f
-// 			break
-// 		}
-// 	}
-// 	return
-// }
-
 // setValue assigns a field value according to its kind/pointer type.
-func setValue(sf reflect.Value, field utils.Field, fieldName, format string, idx []int) error {
+func setValue(sf reflect.Value, field internal.Field, fieldName, format string, idx []int) error {
 	switch sf.Kind() {
 	case reflect.String:
 		sf.SetString(field.Value)
@@ -339,7 +150,7 @@ func setFloat(sf reflect.Value, raw, field string, format string, idx []int) err
 	return nil
 }
 
-func setPointer(sf reflect.Value, field utils.Field, fieldName, format string, idx []int) error {
+func setPointer(sf reflect.Value, field internal.Field, fieldName, format string, idx []int) error {
 	switch sf.Type().Elem().Kind() {
 	case reflect.String:
 		sf.Set(reflect.ValueOf(&field.Value))
@@ -410,7 +221,7 @@ func collectFields(t reflect.Type, parentIndex []int, tags map[string]fieldInfo)
 		if tag == "" {
 			tag = f.Name
 		}
-		keys := utils.SplitQuoted(tag)
+		keys := internal.SplitQuoted(tag)
 		if len(keys) == 0 || keys[0] == "" {
 			continue
 		}
